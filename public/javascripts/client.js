61ab87f004b220f87aff0b413cfd731152dc0798
Mike Kovner <mike.kovner@gmail.com>
2014-03-03 17:20:05 -0800

    init

---
+++ public/javascripts/client.js
@@ -0,0 +1,149 @@
+var messageArray = [];
+var marksArray = [];
+var marksCounter = 0;
+var filterArray = [];
+var filterCounter = 0;
+var viz;
+
+$(function() {
+	// Start with nothing showing. Then when client connects to socket give
+	// an input box for their name
+	$("#messages").hide();
+	$("#input").hide();
+	$("#name").hide();
+
+
+	var socket = io.connect();
+
+	socket.on('connect', function() {
+		$("#connecting").hide();
+		var $nameBox = $("#name");
+		$nameBox.show();
+		$nameBox.focus();
+		$nameBox.keypress(function(key) {
+			// Check if the hit 'enter' If they did, send the name to the Server
+			// and unhide the main UI.
+			if(key.which === 13 && $nameBox.val().length > 0) {
+				socket.emit('name', $nameBox.val());
+				$nameBox.hide();
+				$("#messages").show();
+				$("#input").show();
+				//Put 10 empty message divs inside the message div
+				for(var i = 0; i < 11; i++) {
+					messageArray[i] = $('<div class="blank"></div>');
+					$("#messages").append(messageArray[i]);
+				}
+				//Load the Tableau viz
+				var sheetDiv = document.getElementById("vizDiv");
+				var sheetURL = "http://public.tableausoftware.com/views/CerebralSales/Dashboard1";
+				var sheetOptions = {
+					hideTabs: true,
+					hideToolbar: true,
+					width: "600px",
+					height: "616px",
+					onFirstInteractive: function() {
+					}
+				};
+				viz = new tableauSoftware.Viz(sheetDiv, sheetURL, sheetOptions);
+				viz.addEventListener('marksselection', function(event) {
+					event.getMarksAsync().then(function(marks) {
+						if(marks.length > 0) {
+							sheet = event.getWorksheet().getName();
+							socket.emit('clientMarks', {marks: marks, sheet: sheet});
+						}
+					});
+				});
+				viz.addEventListener('filterchange', function(event) {
+					var sheet = viz.getWorkbook().getActiveSheet().getWorksheets().get("Profit Map");
+					sheet.getFiltersAsync().then(function(filters) {
+						var filtersArray = filters[2].getAppliedValues();
+						var genreArray = [];
+						var i = 0;
+						for(i = 0; i < filtersArray.length; i++) {
+							genreArray[i] = filtersArray[i].value;
+						}
+						socket.emit('clientFilter', {genres: genreArray});
+					});
+				});
+			}
+		});
+	});
+
+	socket.on('serverMessage', function(message) {
+		addMessage(message);
+	});
+	var lastData;
+	socket.on('serverMarks', function(data) {
+		//Because selecting marks on a new sheet activates the markselection event twice
+		//we need to check if we've already seen this selection.
+		if(JSON.stringify(data) === JSON.stringify(lastData)) {
+			return;
+		}
+
+		lastData = data;
+		marksArray[marksCounter] = data;
+		//SocketIO converted our tableauServer.Mark objects into generic Objects.
+		//Let's convert them back to Mark Objects
+		var i;
+		for(i = 0; i < marksArray[marksCounter].marks.length; i++) {
+			var oldMark = marksArray[marksCounter].marks[i];
+			var newMark = new tableauSoftware.Mark();
+			newMark._impl._collection = oldMark._impl._collection;
+			newMark._impl._tupleId = oldMark._impl._tupleId;
+			marksArray[marksCounter].marks[i] = newMark;
+		}
+		var message = data.user + " selected " +
+			"<button class='btn btn-link' onclick='selectMarks(" + marksCounter + ")'>" +
+			data.marks.length + " marks on " + data.sheet + "</button>";
+		marksCounter += 1;
+		addMessage(message);
+	});
+	var lastFilter;
+	socket.on('serverFilter', function(data) {
+		//Because the filter applies to 2 sheets, this fires twice for every filter.
+		//Check if it's the same as the last one.
+		if(JSON.stringify(data) === JSON.stringify(lastFilter)) {
+			return;
+		}
+		lastFilter = data;
+		filterArray[filterCounter] = data;
+		var message = data.user +
+			" <button class='btn btn-link' onclick='changeFilter(" + filterCounter + ")'>" +
+			" filtered to " + data.genres.length + " genres.</button>";
+		filterCounter += 1;
+		addMessage(message);
+	});
+
+	var $inputBox = $("#input");
+	$inputBox.keypress(function(key) {
+		//Check if the hit 'enter'
+		if(key.which === 13 && $inputBox.val().length > 0) {
+			socket.emit('clientMessage', $inputBox.val());
+			$inputBox.val('');
+		}
+	});
+
+	function addMessage(message) {
+		//Move all of the message htmls up one then put the new message in the last
+		for(var i = 0; i < 10; i++) {
+			messageArray[i].html(messageArray[i+1].html());
+			if(messageArray[i].html().length > 1) {
+				messageArray[i].removeClass("blank").addClass("well well-sm message");
+			}
+		}
+		messageArray[10].html(message);
+		messageArray[10].removeClass("blank").addClass("well well-sm message");
+	}
+
+});
+
+
+var selectMarks = function(i) {
+	var sheet = viz.getWorkbook().getActiveSheet().getWorksheets().get(marksArray[i].sheet);
+	sheet.selectMarksAsync(marksArray[i].marks, "REPLACE");
+};
+
+var changeFilter = function(i) {
+	var sheet = viz.getWorkbook().getActiveSheet().getWorksheets().get("Profit Map");
+	sheet.applyFilterAsync("Genre", filterArray[i].genres, "REPLACE");
+};
---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function(req,res) {
+	res.render("index.html");
+});
+
+var io = require('socket.io').listen(app.listen(8888));
+console.log("Listening on port 8888.");
+
+io.sockets.on('connection', function(socket) {
+	// socket.emit('serverMessage', 'Welcome to the chat');
+	socket.on('name', function(name) {
+		socket.name = name;
+		socket.broadcast.emit('serverMessage', name + " joined TabChat.");
+		socket.emit('serverMessage', "Hello " + name + ". Welcome to TabChat");
+	});
+	socket.on('clientMessage', function(data) {
+		io.sockets.emit('serverMessage', socket.name + ": " + data);
+	});
+	socket.on('clientMarks', function(data) {
+		io.sockets.emit('serverMarks', {user: socket.name, marks: data.marks, sheet: data.sheet});
+	});
+	socket.on('clientFilter', function(data) {
+		io.sockets.emit('serverFilter', {user: socket.name, genres: data.genres});
+	});
+});
